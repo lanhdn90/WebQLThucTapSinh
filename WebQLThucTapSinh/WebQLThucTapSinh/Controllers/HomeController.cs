1bf5da380d60cf6264b66c18e2d4e350d82c668b
Lanh Huynh <lanhdn90@gmail.com>
2020-12-02 10:02:35 +0700

    login

--- WebQLThucTapSinh/WebQLThucTapSinh/Controllers/HomeController.cs
+++ WebQLThucTapSinh/WebQLThucTapSinh/Controllers/HomeController.cs
@@ -21,6 +21,91 @@ namespace WebQLThucTapSinh.Controllers
             return View();
         }
 
+        public ActionResult Login(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                WebDatabaseEntities database = new WebDatabaseEntities();
+                var res = database.Users.SingleOrDefault(x => x.UserName == user.UserName);
+                if (res == null)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại.");
+                }
+                else
+                {
+                    if (res.Status == false)
+                    {
+                        ModelState.AddModelError("", "Tài khoản đang bị khóa.");
+                    }
+                    else
+                    {
+                        if (res.PassWord != MaHoaMd5.MD5Hash(user.PassWord))
+                        {
+                            ModelState.AddModelError("", "Mật khẩu không đúng.");
+
+                        }
+                        else
+                        {
+                            Session["TenTk"] = user.UserName;
+                            var role = database.Person.SingleOrDefault(x => x.PersonID == res.PersonID);
+                            Session["Role"] = role.RoleID;
+
+                            // Admin = 1
+                            if (role.RoleID == 1)
+                            {
+                                return RedirectToAction("TTcanhan", "Information");
+                            }
+                            else
+                            {
+                                Session["Person"] = role.PersonID;
+                                // Interns = 5
+                                if (role.RoleID == 5)
+                                {
+                                    return RedirectToAction("TTcanhan", "Information");
+                                }
+                                else
+                                {
+                                    Session["CompanyID"] = role.CompanyID;
+                                    if (role.RoleID == 3)
+                                    {
+                                        //Giảng viên = 3
+                                        Session["SchoolID"] = role.SchoolID;
+                                        return RedirectToAction("TTcanhan", "Information");
+                                    }
+                                    else
+                                    {
+
+                                        if (role.RoleID == 2)
+                                        { // Manager
+                                            return RedirectToAction("TTcanhan", "Information");
+                                        }
+                                        else
+                                        {// Leader or school
+                                            return RedirectToAction("TTcanhan", "Information");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            ViewBag.ListCom = new Share().listOrgan(2).ToList();
+            ViewBag.ListSchool = new Share().listOrgan(3).ToList();
+            return View("Index");
+            //return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult DangXuat()
+        {
+            Session["TenTk"] = null;
+            Session["Role"] = null;
+            Session["Person"] = null;
+            Session["SchoolID"] = null;
+            Session["CompanyID"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
         public void checkEDate()
         {
             checkOrgan();
@@ -214,89 +299,138 @@ namespace WebQLThucTapSinh.Controllers
             database.SaveChanges();
         }
 
-        public ActionResult Login(Users user)
+        [HttpGet]
+        public ActionResult PasswordRetrieval()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PasswordRetrieval(RestartPassword restart)
         {
             if (ModelState.IsValid)
             {
                 WebDatabaseEntities database = new WebDatabaseEntities();
-                var res = database.Users.SingleOrDefault(x => x.UserName == user.UserName);
-                if (res == null)
+                // Kiểm tra Object User có tồn tại hay không
+                var model = database.Users.Find(restart.UserName);
+                if (model == null)
                 {
-                    ModelState.AddModelError("", "Tài khoản không tồn tại.");
+                    ModelState.AddModelError("", "UserName không tồn tại");
                 }
                 else
                 {
-                    if (res.Status == false)
-                    {
-                        ModelState.AddModelError("", "Tài khoản đang bị khóa.");
-                    }
-                    else
-                    {
-                        if (res.PassWord != MaHoaMd5.MD5Hash(user.PassWord))
-                        {
-                            ModelState.AddModelError("", "Mật khẩu không đúng.");
 
+                    var findPer = database.Person.Find(model.PersonID);
+                    if (findPer.Email == restart.Email)
+                    {
+                        var newPass = new Share().RandomText();
+                        if (SendPass(findPer.PersonID, newPass))
+                        {
+                            UpdateUser(findPer.PersonID, newPass);
                         }
                         else
                         {
-                            Session["TenTk"] = user.UserName;
-                            var role = database.Person.SingleOrDefault(x => x.PersonID == res.PersonID);
-                            Session["Role"] = role.RoleID;
-
-                            // Admin = 1
-                            if (role.RoleID == 1)
-                            {
-                                return RedirectToAction("TTcanhan", "Information");
-                            }
-                            else
-                            {
-                                Session["Person"] = role.PersonID;
-                                // Interns = 5
-                                if (role.RoleID == 5)
-                                {
-                                    return RedirectToAction("TTcanhan", "Information");
-                                }
-                                else
-                                {
-                                    Session["CompanyID"] = role.CompanyID;
-                                    if (role.RoleID == 3)
-                                    {
-                                        //Giảng viên = 3
-                                        Session["SchoolID"] = role.SchoolID;
-                                        return RedirectToAction("TTcanhan", "Information");
-                                    }
-                                    else
-                                    {
-                                        
-                                        if (role.RoleID == 2)
-                                        { // Manager
-                                            return RedirectToAction("TTcanhan", "Information");
-                                        }
-                                        else
-                                        {// Leader or school
-                                            return RedirectToAction("TTcanhan", "Information");
-                                        }
-                                    }
-                                }
-                            }
+                            ModelState.AddModelError("", "Lỗi gửi Email");
                         }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Email không chính xác!");
                     }
                 }
             }
-            ViewBag.ListCom = new Share().listOrgan(2).ToList();
-            ViewBag.ListSchool = new Share().listOrgan(3).ToList();
-            return View("Index");
-            //return RedirectToAction("Index", "Home");
+            return View("PasswordRetrieval");
         }
 
-        public ActionResult DangXuat()
+        [HttpGet]
+        public ActionResult CreateUser()
         {
-            Session["TenTk"] = null;
-            Session["Role"] = null;
-            Session["Person"] = null;
-            Session["SchoolID"] = null;
-            Session["CompanyID"] = null;
-            return RedirectToAction("Index", "Home");
+            return View();
+        }
+
+        public ActionResult CreateUser(Users user)
+        {
+            WebDatabaseEntities database = new WebDatabaseEntities();
+            var findU = database.Users.Find(user.UserName);
+            if (findU != null)
+            {
+                ModelState.AddModelError("", "UserName đã tồn tại");
+            }
+            else
+            {
+                var findP = database.Person.Find(user.PersonID);
+                if (findP == null)
+                {
+                    ModelState.AddModelError("", "Sai mã xác nhận");
+                }
+                else
+                {
+                    Users tk = new Users();
+                    tk.UserName = user.UserName;
+                    tk.PassWord = MaHoaMd5.MD5Hash(user.PassWord);
+                    tk.PersonID = user.PersonID;
+                    tk.Status = true;
+                    database.Users.Add(tk);
+                    database.SaveChanges();
+                    ModelState.AddModelError("", "Đăng ký thành công");
+                }
+            }
+            return View("CreateUser");
+        }
+
+        public void UpdateUser(string personID, string newPass)
+        {
+            WebDatabaseEntities database = new WebDatabaseEntities();
+            var model = database.Users.SingleOrDefault(x => x.PersonID == personID);
+            model.PassWord = MaHoaMd5.MD5Hash(newPass);
+            database.SaveChanges();
+
+        }
+
+        public bool SendPass(string id, string newPass)
+        {
+            try
+            {
+                WebDatabaseEntities database = new WebDatabaseEntities();
+                var res = database.Person.Find(id);
+                string cus = res.LastName + " " + res.FirstName;
+                string email = res.Email;
+                string nd = newPass;
+                string content = System.IO.File.ReadAllText(Server.MapPath("~/Email/EmailPassword.html"));
+                content = content.Replace("{{CustomerName}}", cus);
+                content = content.Replace("{{noidung}}", nd);
+
+                var fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"].ToString();
+                var fromEmailPassword = ConfigurationManager.AppSettings["FromEmailPassword"].ToString();
+                var smtpHost = ConfigurationManager.AppSettings["SMTPHost"].ToString();
+
+                MailMessage message = new MailMessage(fromEmailAddress, email);
+                message.Subject = "Cấp mật khẩu mới";
+                message.IsBodyHtml = true;
+                message.Body = content;
+
+                SmtpClient client = new SmtpClient(smtpHost, 587);
+                client.EnableSsl = true;
+                client.Timeout = 100000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                // Đăng nhập gmail
+                NetworkCredential nc = new NetworkCredential(fromEmailAddress, fromEmailPassword);
+                client.UseDefaultCredentials = false;
+                client.Credentials = nc;
+                client.Send(message);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public ActionResult cvOrgan(string id)
+        {
+            WebDatabaseEntities database = new WebDatabaseEntities();
+            var model = database.Organization.SingleOrDefault(x => x.ID == id);
+            return View(model);
         }
     }
 }
